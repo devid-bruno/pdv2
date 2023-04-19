5840b8fffbdc0789c07609742a89d4a901815e78
mac <mac@llfl.local>
2023-04-19 13:19:53 -0300

    resumo financeiro

---
+++ app/Http/Controllers/VendaRapidaController.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\StoreVendaRapidaRequest;
+use App\Http\Requests\UpdateVendaRapidaRequest;
+use App\Models\VendaRapida;
+
+class VendaRapidaController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(StoreVendaRapidaRequest $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(VendaRapida $vendaRapida)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(VendaRapida $vendaRapida)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(UpdateVendaRapidaRequest $request, VendaRapida $vendaRapida)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(VendaRapida $vendaRapida)
+    {
+        //
+    }
+}
